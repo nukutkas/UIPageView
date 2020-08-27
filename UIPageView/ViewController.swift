4e26d0f7eb5fcc52e731bbe71e3723157075ccfb
Nikita Kochetov <nothsmple@mail.ru>
2020-08-27 09:02:31 +0300

    Initial Commit

---
+++ UIPageView/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  UIPageView
+//
+//  Created by Татьяна Кочетова on 27.08.2020.
+//  Copyright © 2020 Nikita Kochetov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
